a85a5badd891b3883f93aa23bd29d36c48d48473
ddlucad96 <ddlucad@gmail.com>
2018-01-12 10:38:54 +0100

    Crea applicazione

---
+++ index.js
@@ -0,0 +1,70 @@
+/*globals require, console, process */
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// instantiate express
+const app = express();
+
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// set our port
+var port = process.env.PORT || 8080;
+
+
+// get an instance of the express Router
+var router = express.Router();
+
+var astronauti = [];
+
+
+// test route to make sure everything is working
+router.get('/', function(req, res) {
+    //res.json({ message: 'welcome to our api!' });
+
+    var risposta = {
+        mess: "Ciao"
+    }
+
+    res.json(risposta);
+
+});
+
+
+// middleware route to support CORS and preflighted requests
+app.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    //Enabling CORS
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Content-Type', 'application/json');
+    if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+        return res.status(200).json({});
+    }
+    // make sure we go to the next routes
+    next();
+});
+
+// register our router on /api
+app.use('/api/v1', router);
+
+// handle invalid requests and internal error
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({ error: { message: err.message } });
+});
+
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
